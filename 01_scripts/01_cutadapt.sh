ed533fe0a7a4ed3e48ad12f6c2abfaa59b7190e5
bensutherland <bensutherland7@gmail.com>
2015-08-05 13:08:49 -0400

    improved enviro variables for cutadapt

--- 01_scripts/01_cutadapt.sh
+++ 01_scripts/01_cutadapt.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
 # remove adapter sequences from reads using cutadapt
-# point to the trimmomatic.jar in Global variables
 
 # Global variables
 RAW_FOLDER="02_raw_data"
-ADREM_FOLDER="03_adapter_removed"
+REMADAPT_FOLDER="03_adapter_removed"
 CUTADAPT="/usr/local/bin/cutadapt"
+ADAPTER_FWD="AGATCGGAAGAGCACACGTCTGAACTCCAGTCAC"
+ADAPTER_REV="AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGTAGATCTCGGTGGTCGCCGTATCATT"
 
-# Filtering and trimming data with trimmomatic
+# remove adapters with cutadapt
 ls -1 $RAW_FOLDER/*.fastq.gz | \
-    perl -pe 's/R[12]\_001.fastq\.gz//' | \
     grep -vE "paired|single" | \
+    perl -pe 's/R[12]\.fastq\.gz//' | \
     sort -u | \
     while read i
     do
       echo $i
       $CUTADAPT \
-        -a AGATCGGAAGAGCACACGTCTGAACTCCAGTCAC \
-        -A AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGTAGATCTCGGTGGTCGCCGTATCATT \
-        -o "$i"R1_001_remadapt.fastq.gz -p "$i"R2_001_remadapt.fastq.gz \
-	"$i"R1_001.fastq.gz "$i"R2_001.fastq.gz \
-        --minimum-length 2
+        -a $ADAPTER_FWD \
+        -A $ADAPTER_REV \
+        -o "$i"R1.remadapt.fastq.gz -p "$i"R2.remadapt.fastq.gz \
+	"$i"R1.fastq.gz "$i"R2.fastq.gz \
+        --minimum-length 100 \
+        -e 0.2
     done
 
 # Transfer trimmed files to $ADREM_FOLDER folder
-mv $RAW_FOLDER/*remadapt* $ADREM_FOLDER
+mv $RAW_FOLDER/*remadapt* $REMADAPT_FOLDER
