a542d163e58b156c0e2a0d738efd0f4f712d24a2
bensutherland <bensutherland7@gmail.com>
2015-09-03 16:05:52 -0400

    using FLASH merger

--- 01_scripts/02_flash_merge.sh
+++ 01_scripts/02_flash_merge.sh
@@ -2,29 +2,25 @@
 # decompresses paired fastq.gz files and uses PEAR to collapse into single reads when possible
 # removes decompressed reads, then moves files to the binned folder
 
+
+# from FLASH website http://ccb.jhu.edu/software/FLASH/
+# "Merging mate pairs by FLASH as a pre-processor for genome assembly yields singificantly higher N50 value of contigs and scaffolds. It also reduces the number of missassembled contigs."
+# FLASH is not designed for data with significant indel error, it is best suited for Illumina (see README for more information)
+# gzip compressed input is autodetected
+
 # Global variables
 ADREM_FOLDER="03_adapter_removed"
 BINNED_FOLDER="04_binned_pairs"
-PEAR="/usr/local/bin/pear"
+FLASH="/project/lbernatchez/drobo/users/bensuth/programs/FLASH-1.2.11/flash"
+#PEAR="/usr/local/bin/pear"
 
 ## Filtering and trimming data with trimmomatic
-ls -1 $ADREM_FOLDER/*.fastq.gz | \
-    perl -pe 's/R[12]\_001_remadapt.fastq\.gz//' | \
-    grep -vE "paired|single" | \
-    sort -u | \
+ls -1 $ADREM_FOLDER/*.fastq.gz | 
+    perl -pe 's/R[12]\.remadapt.fastq\.gz//' | 
+    sort -u | 
     while read i
     do
-      echo $i \
-     # Decompress reads
-      echo "Decompressing Reads"
-      gunzip -c "$i"R1_001_remadapt.fastq.gz > "$i"R1_001_remadapt.fastq
-      gunzip -c "$i"R2_001_remadapt.fastq.gz > "$i"R2_001_remadapt.fastq
-      $PEAR -f "$i"R1_001_remadapt.fastq -r "$i"R2_001_remadapt.fastq -o "$i"output \
-   # Remove Decompressed Reads
-     #echo "Removing decompressed reads"
-     #rm "$i"R1_001_remadapt.fastq
-     #rm "$i"R2_001_remadapt.fastq
+      echo $i 
+      $FLASH "$i"R1.remadapt.fastq.gz "$i"R2.remadapt.fastq.gz --compress --output-directory=$BINNED_FOLDER -t 5 
     done
 
-mv $ADREM_FOLDER/*assembled* $BINNED_FOLDER/ 
-mv $ADREM_FOLDER/*discarded* $BINNED_FOLDER/
--- 01_scripts/jobs/02_flash_merge_job.sh
+++ 01_scripts/jobs/02_flash_merge_job.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#$ -N PEAR
+#$ -N FLASH
 #$ -M $MY_EMAIL_ADDRESS
 #$ -m beas
-#$ -pe smp 3
-#$ -l h_vmem=40G
+#$ -pe smp 5
+#$ -l h_vmem=20G
 #$ -l h_rt=24:00:00
 #$ -cwd
 #$ -S /bin/bash
 
-time ./01_scripts/02_PEAR.sh
+time ./01_scripts/02_flash_merge.sh
