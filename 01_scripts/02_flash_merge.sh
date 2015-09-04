6a50a4daf77cd92fa4aaf74b3bc1d64b56e41141
bensutherland <bensutherland7@gmail.com>
2015-09-04 10:00:29 -0400

    fixed basename issue

--- 01_scripts/02_flash_merge.sh
+++ 01_scripts/02_flash_merge.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# decompresses paired fastq.gz files and uses PEAR to collapse into single reads when possible
-# removes decompressed reads, then moves files to the binned folder
 
+# Uses the program FLASh to merge overlapping paired-end reads to produce single contigs of a greater length
+# default overlap is 10 bp
 
 # from FLASH website http://ccb.jhu.edu/software/FLASH/
 # "Merging mate pairs by FLASH as a pre-processor for genome assembly yields singificantly higher N50 value of contigs and scaffolds. It also reduces the number of missassembled contigs."
@@ -12,7 +12,6 @@
 ADREM_FOLDER="03_adapter_removed"
 BINNED_FOLDER="04_binned_pairs"
 FLASH="/project/lbernatchez/drobo/users/bensuth/programs/FLASH-1.2.11/flash"
-#PEAR="/usr/local/bin/pear"
 
 ## Filtering and trimming data with trimmomatic
 ls -1 $ADREM_FOLDER/*.fastq.gz | 
@@ -21,6 +20,6 @@ ls -1 $ADREM_FOLDER/*.fastq.gz |
     while read i
     do
       echo $i 
-      $FLASH "$i"R1.remadapt.fastq.gz "$i"R2.remadapt.fastq.gz --compress --output-directory=$BINNED_FOLDER -t 5 --output-prefix="$i"
+      prefix=$(basename $i) 
+      $FLASH "$i"R1.remadapt.fastq.gz "$i"R2.remadapt.fastq.gz --compress --output-directory=$BINNED_FOLDER -t 5 --output-prefix="$prefix"
     done
-
